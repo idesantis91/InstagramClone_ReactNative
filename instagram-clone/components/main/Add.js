b2766e1e8ee1c0fa4ab6937ac2d01adde4eae62d
Isaiah DeSantis <42743832+idesantis91@users.noreply.github.com>
2021-05-25 14:56:04 -0400

    Added Camera

--- instagram-clone/components/main/Add.js
+++ instagram-clone/components/main/Add.js
@@ -1,10 +1,91 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, Image, Button } from 'react-native';
+import { Camera } from 'expo-camera';
 
-export default function Add() {
-    return (
-        <View>
-            <Text>Add Screen</Text>
-        </View>
-    )
+export default function App() {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [camera, setCamera] = useState(null);
+  const [image, setImage] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  const takePicture = async() =>{
+    if(camera){
+        const data = await camera.takePictureAsync(null);
+        // console.log(data.uri)
+        setImage(data.uri)
+    }
+  }
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+  return (
+    <View style={styles.container}>
+      <View style={styles.cameraContainer}>
+        <Camera 
+            ref ={ref => setCamera(ref)}
+            style={styles.fixedRatio} 
+            type={type}
+            ratio={'1:1'}    
+            />
+      </View>
+    <Button
+            title="Flip"
+            onPress={() => {
+              setType(
+                type === Camera.Constants.Type.back
+                  ? Camera.Constants.Type.front
+                  : Camera.Constants.Type.back
+              );
+            }}>
+          </Button>
+          <Button
+              title="Take Picture"
+              onPress={() => takePicture()}
+          />
+          {image && <Image source={{uri: image}} style={{flex:1}}/>}
+    </View>
+  );
 }
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+    },
+    cameraContainer:{
+        flex: 1,
+        flexDirection: 'row'
+    },
+    fixedRatio:{
+        flex: 1,
+        aspectRatio: 1
+    },
+    camera: {
+      flex: 1,
+    },
+    buttonContainer: {
+      flex: 1,
+      backgroundColor: 'transparent',
+      flexDirection: 'row',
+      margin: 20,
+    },
+    button: {
+      flex: 0.1,
+      alignSelf: 'flex-end',
+      alignItems: 'center',
+    },
+    text: {
+      fontSize: 18,
+      color: 'white',
+    },
+  });
+  
